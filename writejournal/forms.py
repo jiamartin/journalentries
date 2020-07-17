889454677d3f5788111caca3efe53790bb12e981
jiamartin <julialagarde3@gmail.com>
2020-07-17 22:46:09 +0800

    07/17

---
+++ writejournal/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from . models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'h-full-width'}),
+            'title_tag': forms.TextInput(attrs={'class': 'h-full-width'}),
+            'author': forms.Select(attrs={'class': 'h-full-width'}),
+            'body': forms.Textarea(attrs={'class': 'h-full-width'}),
+        }
--- writejournal/views.py
+++ writejournal/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from . models import Post
+from . forms import PostForm
 
 
 class indexView(ListView):
@@ -13,6 +14,6 @@ class entryView(DetailView):
 
 class addView(CreateView):
         model = Post
+        form_class = PostForm
         template_name = 'add.html'
-        fields = '__all__'
 
